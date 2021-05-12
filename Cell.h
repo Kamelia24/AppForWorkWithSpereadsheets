58bc852a78810f55c8df314d8af22b6dc75d220c
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2021-05-12 12:35:42 +0300

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -35,15 +35,6 @@ class CellValue
     // Instance of class CellValue might contain both value and formula. When saving only the formula is saved.
     // Values of the cells are calculated runtime.
 };
-/*
-class CellFormula // за пръв път срещам клас без член данни, трябва ли да му се добавят някакви или да се слее с друг клас който има данни
-{
-    public:
-    CellFormula(char* formulaString);// конструктор
-    void setAddress(char* address); // функция която чете файла и намира клетките отговарящи на адрес от формулата?
-    CellValueHolder calculate(); // тук пак това с union, функция която пресмята стойността спрямо зададената формула за клетката?
-};
-*/
 
 class Cell
 {
