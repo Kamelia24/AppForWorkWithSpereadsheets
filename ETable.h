035bd26e337a0c26385afdb0a170ea5a9552a366
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2021-05-12 11:30:40 +0300

    Create ETable.h

---
+++ ETable.h
@@ -0,0 +1,26 @@
+#ifndef ETABLEH
+#define ETABLEH
+
+#include <string>
+
+using namespace std;
+class ETable
+{
+    private:
+    string lastError = "";
+    string folderForFiles = ".\\Data"; // Here the data files will be created.
+    string activeFilePath = ""; // The file from which data is read or saved.
+    public:
+    ETable(string fileName);
+    ~ETable();
+    bool readRow(string address);
+    bool readRow(int rowNumber);
+    bool saveRow(string address);
+    bool saveRow(int rowNumber);
+    string getLastError();
+    void print();
+    void createTestData();
+    void recognizeTestData();
+};
+
+#endif // ETABLEH
