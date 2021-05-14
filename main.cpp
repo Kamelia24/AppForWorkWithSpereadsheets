dcf319f8361950696d3ae36b03f4f634ab62fde9
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2021-05-14 11:36:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <windows.h>
 //#include <conio.h>
-#include<string>
+#include <string>
 
 #include "ETable.cpp"
 
 using namespace std;
 
-void setBulgarianText() {
+void setBulgarianText()
+{
     SetConsoleCP(65001); // UTF-8
     SetConsoleOutputCP(65001);
 }
@@ -17,36 +18,51 @@ int main()
     setBulgarianText();
     string fileName;
     string rowNum;
-    do{
-    cout<<"Create file: \n";
-    cin>>fileName;
-    if(fileName.find('.')!=string::npos) cout<<"you can't use '.' in filename! \n";
-    if(doesFileExist(".\\Data/"+fileName+".txt")==1) cout<<"such file already exists! \n";
-    }while(fileName.find('.')!=string::npos || doesFileExist(".\\Data/"+fileName+".txt")==1);
-    ETable *eTable = new ETable(fileName+".txt");
+    string value;
+    string address;
+    do
+    {
+        cout << "Create file: \n";
+        cin >> fileName;
+        if (fileName.find('.') != string::npos)
+            cout << "you can't use '.' in filename! \n";
+        if (doesFileExist(".\\Data/" + fileName + ".txt") == 1)
+            cout << "such file already exists! \n";
+    } while (fileName.find('.') != string::npos || doesFileExist(".\\Data/" + fileName + ".txt") == 1);
+    ETable *eTable = new ETable(fileName + ".txt");
     char operation = 'N';
-    do {
+    do
+    {
         cout << "Choose what to do with the file:" << endl;
-        cout << "To see the table type: P/p" << endl;
-        cout << "To change sell value type: E/e" << endl;
+        cout << "To see the table: P/p" << endl;
+        cout << "To change row: ER/er" << endl;
+        cout << "To change cell value: EC/ec" << endl;
         // Not required. Usefull for testing:
-        cout << "To create test data type: C/c" << endl;
-        cout << "To see type of data in the table type: R/r" << endl;
+        cout << "To create test data: C/c" << endl;
+        cout << "To see type of data in the table: R/r" << endl;
         //
         cout << "To Exit type: Q/q" << endl;
         cin >> operation; //operation = getch();
-        switch(operation) {
+        switch (operation)
+        {
         case 'P':
         case 'p':
             eTable->print();
             break;
-        case 'E':
-        case 'e':
-            cout<<"choose row (ex: R1 or 1)";
-            cin>>rowNum;
-            cout<<typeid(rowNum).name();
+        case 'ER':
+        case 'er':
+            cout << "choose row (ex: R1 or 1)";
+            cin >> rowNum;
+            cout << typeid(rowNum).name();
             //if the addres is number convert it to int
             eTable->saveRow(rowNum);
+            break;
+        case 'EC':
+        case 'ec':
+            cout << "choose cell (ex: R1C2 or 1)";
+            cin >> address;
+            cin >> value;
+            //Cell->changeValue(address,value);
             break;
         case 'C':
         case 'c':
@@ -57,7 +73,7 @@ int main()
             eTable->recognizeTestData();
             break;
         default:
-            cout<<"Error 404!Command not found!";
+            cout << "Error 404!Command not found!";
         }
     } while (operation != 'Q' && operation != 'q');
     delete eTable;
