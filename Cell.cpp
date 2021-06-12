ab999361adbffeec04fdea96b54417bca1cd3a0d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2021-06-13 01:25:43 +0300

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -7,22 +7,6 @@ using namespace std;
 
 const char STRING_IDENTIFIER = '"';
 
-CellValueHolder::CellValueHolder()
-{
-}
-
-CellValueHolder::~CellValueHolder()
-{
-}
-
-CellValue::CellValue()
-{
-}
-
-CellValue::~CellValue()
-{
-}
-
 Cell::Cell()
 {
 }
@@ -67,12 +51,13 @@ string Cell::getValueTypeAsString()
     return result;
 }
 
-bool isLongValue(const string stringValue, long *outValue)
+///function which returns the type of the value in a cell
+CellValueType getValueType(const string stringValue)
 {
     size_t startPos = 0;
     if (stringValue == "")
     {
-        return false;
+        return CellValueType::EMPTY_VALUE;
     }
     if (stringValue.at(0) == '-' || stringValue.at(0) == '+')
     {
@@ -85,36 +70,6 @@ bool isLongValue(const string stringValue, long *outValue)
             return false;
         }
     }
-    try
-    {                                  //we try to run the code if there are any errors the catch is activated
-        *outValue = stol(stringValue); //parses string to long value
-        return true;
-    }
-    catch (const invalid_argument &ex)
-    {
-        return false;
-    }
-    catch (const out_of_range &ex)
-    {
-        return false;
-    }
-    catch (const std::runtime_error &ex)
-    {
-        return false;
-    }
-}
-
-bool isDoubleValue(const string stringValue, double *outValue)
-{
-    size_t startPos = 0;
-    if (stringValue == "")
-    {
-        return false;
-    }
-    if (stringValue.at(0) == '-' || stringValue.at(0) == '+')
-    {
-        startPos = 1;
-    }
     // Check case 12.4.44
     int dotOccurencies = 0;
     for (size_t pos = startPos; pos < stringValue.size(); pos++)
@@ -126,8 +81,9 @@ bool isDoubleValue(const string stringValue, double *outValue)
     }
     if (dotOccurencies > 1)
     {
-        return false;
+        return CellValueType::EMPTY_VALUE;
     }
+
     for (size_t pos = startPos; pos < stringValue.size(); pos++)
     {
         if (stringValue.at(pos) == '.')
@@ -136,25 +92,16 @@ bool isDoubleValue(const string stringValue, double *outValue)
         }
         else if (stringValue.at(pos) < '0' || stringValue.at(pos) > '9')
         {
-            return false;
+            return CellValueType::EMPTY_VALUE;
         }
     }
-    try
+    if (dotOccurencies == 1)
     {
-        *outValue = stod(stringValue); //parses string value as float value
-        return true;
+        return CellValueType::DOUBLE_VALUE;
     }
-    catch (const invalid_argument &ex)
+    else
     {
-        return false;
-    }
-    catch (const out_of_range &ex)
-    {
-        return false;
-    }
-    catch (const std::runtime_error &ex)
-    {
-        return false;
+        return CellValueType::LONG_VALUE;
     }
 }
 
@@ -163,32 +110,15 @@ bool Cell::setValue(const string stringValue)
     long longValue;
     double doubleValue;
 
-    if (stringValue.size() == 0)
-    {
-        value.type = CellValueType::EMPTY_VALUE;
-        value.value.stringValue = stringValue;
-        return true;
-    }
-    if (isLongValue(stringValue, &longValue))
-    {
-        value.type = CellValueType::LONG_VALUE;
-        value.value.longValue = longValue;
-        return true;
-    }
-    if (isDoubleValue(stringValue, &doubleValue))
-    {
-        value.type = CellValueType::DOUBLE_VALUE;
-        value.value.doubleValue = doubleValue;
-        return true;
-    }
+    type=getValueType(const string stringValue);
     if (stringValue.at(0) == STRING_IDENTIFIER && stringValue.at(stringValue.size() - 1) == STRING_IDENTIFIER)
     {
-        value.type = CellValueType::STRING_VALUE;
+        type = CellValueType::STRING_VALUE;
         // Remove the " at the beginning and at the end:
         string clearedStringValue = stringValue;
         clearedStringValue = clearedStringValue.erase(0, 1);
         clearedStringValue = clearedStringValue.erase(clearedStringValue.size() - 1, 1);
-        value.value.stringValue = clearedStringValue;
+        value = clearedStringValue;
         //
         cout << stringValue << " " << clearedStringValue << endl;
         //
@@ -196,18 +126,19 @@ bool Cell::setValue(const string stringValue)
     }
     if (stringValue.at(0) == '=')
     {
-        value.type = CellValueType::FORMULA;
-        value.value.formula = stringValue;
+        type = CellValueType::FORMULA;
+        value = stringValue;
         //
         cout << stringValue << endl;
         //
         return true;
     }
     // Value is not recognized. Raise error:
-    value.type = CellValueType::EMPTY_VALUE;
+    type = CellValueType::EMPTY_VALUE;
     lastError = "Value is not recognized: " + stringValue;
     return false;
 }
+///function which converts the formula to a number
 template <typename T>
 T Cell::FormulaResult(string formula)
 { //returns the value of the formula (ako ne e s adresi)
