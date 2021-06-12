769de67e5ba05fde87c1eb9ba6f27404062f390d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2021-06-13 01:25:57 +0300

    Update ETable.h

--- ETable.h
+++ ETable.h
@@ -1,12 +1,22 @@
 #ifndef ETABLEH
 #define ETABLEH
 
-#include <string>
+#include <fstream>
+#include <cstring>
+#include <algorithm> //.find
+#include <cctype> // .isspace
+#include <locale> // for the above include
+#include <iomanip>
+
+#include "string.h"
+#include "Cell.h"
 
 using namespace std;
+///class which contains info of the table.
 class ETable
 {
 private:
+Cell cells[50][50];
     string lastError = "";
     string folderForFiles = ".\\Data"; // Here the data files will be created.
     string activeFilePath = "";        // The file from which data is read or saved.
